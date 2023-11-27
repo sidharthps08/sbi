a0228a90ad767426a7baa41c4a7004f524cfcb9c
sidharthps08 <123804109+sidharthps08@users.noreply.github.com>
2023-11-27 13:07:00 +0530

    third edit

--- calc.py
+++ calc.py
@@ -11,17 +11,7 @@ while True:
     # take input from the user
     choice = input("Enter choice(1/2/3/4): ")
 
-    # check if choice is one of the four options
-    if choice in ('1', '2', '3', '4'):
-        try:
-            num1 = float(input("Enter first number: "))
-            num2 = float(input("Enter second number: "))
-        except ValueError:
-            print("Invalid input. Please enter a number.")
-            continue
-
-        if choice == '1':
-            print(num1, "+", num2, "=", add(num1, num2))
+    # check if cho, "=", add(num1, num2))
 
         elif choice == '2':
             print(num1, "-", num2, "=", subtract(num1, num2))
