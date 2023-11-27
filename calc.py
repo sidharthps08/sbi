5566fa1de891cab611cef99998bd806413f211e8
sidharthps08 <123804109+sidharthps08@users.noreply.github.com>
2023-11-27 13:06:36 +0530

    first edit

--- calc.py
+++ calc.py
@@ -1,20 +1,6 @@
 # This function adds two numbers
 def add(x, y):
     return x + y
-
-# This function subtracts two numbers
-def subtract(x, y):
-    return x - y
-
-# This function multiplies two numbers
-def multiply(x, y):
-    return x * y
-
-# This function divides two numbers
-def divide(x, y):
-    return x / y
-
-
 print("Select operation.")
 print("1.Add")
 print("2.Subtract")
